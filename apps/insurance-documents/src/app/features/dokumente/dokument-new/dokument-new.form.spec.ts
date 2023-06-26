4833e7825c9c3ce3a93abb54408a08c374235d3c
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-06-26 14:49:20 +0200

    test: add test showcasing childLoader

--- apps/insurance-documents/src/app/features/dokumente/dokument-new/dokument-new.form.spec.ts
+++ apps/insurance-documents/src/app/features/dokumente/dokument-new/dokument-new.form.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatSelectHarness } from '@angular/material/select/testing';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
-import { MatErrorHarness } from "@angular/material/form-field/testing";
+import { MatErrorHarness } from '@angular/material/form-field/testing';
 
 describe('DokumentNewForm', () => {
   describe('When "Berechnungsarten" are present', () => {
     it('makes "Berechnungsarten" selectable', async () => {
       await TestBed.configureTestingModule({
         imports: [DokumentNewForm],
-        providers: [provideNoopAnimations()]
+        providers: [provideNoopAnimations()],
       }).compileComponents();
 
       const fixture = TestBed.createComponent(DokumentNewForm);
@@ -19,7 +19,7 @@ describe('DokumentNewForm', () => {
       fixture.componentInstance.voreinstellungen = {
         berechnungsarten: ['Anzahl Mitarbeiter', 'Risiko'],
         risiken: [],
-        zusatzaufschlaege: []
+        zusatzaufschlaege: [],
       };
 
       fixture.detectChanges();
@@ -40,7 +40,7 @@ describe('DokumentNewForm', () => {
     it('displays a validation message', async () => {
       await TestBed.configureTestingModule({
         imports: [DokumentNewForm],
-        providers: [provideNoopAnimations()]
+        providers: [provideNoopAnimations()],
       }).compileComponents();
 
       const fixture = TestBed.createComponent(DokumentNewForm);
@@ -49,7 +49,7 @@ describe('DokumentNewForm', () => {
       fixture.componentInstance.voreinstellungen = {
         berechnungsarten: ['Anzahl Mitarbeiter', 'Risiko'],
         risiken: [],
-        zusatzaufschlaege: []
+        zusatzaufschlaege: [],
       };
 
       fixture.detectChanges();
@@ -62,13 +62,50 @@ describe('DokumentNewForm', () => {
       await select.close();
 
       const error = await loader.getHarness(
-        MatErrorHarness.with({ selector: '[data-test=berechnungsart-select-error]'})
-      )
+        MatErrorHarness.with({
+          selector: '[data-test=berechnungsart-select-error]',
+        })
+      );
 
       const errorText = await error.getText();
 
       expect(errorText).toBe('Bitte wählen Sie eine Berechnungsart aus.');
+    });
+  });
+});
 
-    })
+describe('[👶🏻 Child Loader] Dokument New Form', () => {
+  it('allows having a single test entry from which we access test harnesses', async () => {
+    await TestBed.configureTestingModule({
+      imports: [DokumentNewForm],
+      providers: [provideNoopAnimations()],
+    }).compileComponents();
+
+    const fixture = TestBed.createComponent(DokumentNewForm);
+
+    fixture.componentInstance.voreinstellungen = {
+      berechnungsarten: ['Anzahl Mitarbeiter', 'Risiko'],
+      risiken: [],
+      zusatzaufschlaege: [],
+    };
+
+    fixture.detectChanges();
+
+    const loader = TestbedHarnessEnvironment.loader(fixture);
+
+    const fieldLoader = await loader.getChildLoader(
+      '[data-test=berechnungsart-field]'
+    );
+
+    const select = await fieldLoader.getHarness(MatSelectHarness);
+    await select.open();
+    await select.close();
+
+    // It is not possible to arrange the test before.
+    // <mat-error> needs to be present in the DOM otherwise the test crashes.
+    const error = await fieldLoader.getHarness(MatErrorHarness);
+    const errorMessage = await error.getText();
+
+    expect(errorMessage).toBe('Bitte wählen Sie eine Berechnungsart aus.');
   });
 });
