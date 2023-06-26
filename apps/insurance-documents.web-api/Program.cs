578da0598a9093eb864f9ff70ff302fee50631e2
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-06-26 06:49:52 +0200

    fix(dotnet): register repository

--- apps/insurance-documents.web-api/Program.cs
+++ apps/insurance-documents.web-api/Program.cs
@@ -1,6 +1,22 @@
+using InsuranceDocumentsInfrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DokumentDbContext>(options => options.UseSqlite("Data Source=DokumenteDB.db"));
+builder.Services.AddScoped<IDokumentRepository, DokumentRepository>();
+
+builder.Services.AddCors(options =>
+{
+  options.AddDefaultPolicy(
+    policy  =>
+    {
+      policy.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +24,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateAsyncScope())
+{
+  var dbContext = scope.ServiceProvider.GetRequiredService<DokumentDbContext>();
+  await dbContext.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,6 +39,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
